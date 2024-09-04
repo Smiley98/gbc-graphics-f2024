c7eb89c4c2811a5bccafbea741cee1e3b9d7569e
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-04 17:11:16 -0400

    Added key logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
 
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 720;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main(void)
 {
@@ -24,6 +27,8 @@ int main(void)
     /* Load OpenGL 4.6 (Windows ships with GL 1.0, we don't like GL 1.0)*/
     gladLoadGL();
 
+    glfwSetKeyCallback(window, key_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -40,4 +45,13 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    const char* name = glfwGetKeyName(key, scancode);
+    if (action == GLFW_PRESS)
+        printf("%s\n", name);
+    //if (key == GLFW_KEY_E && action == GLFW_PRESS)
+    //    activate_airship();
 }
